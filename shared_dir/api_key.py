cfaa362ee5aa841058f37d66c22bd555e9f8c6a1
higashi <233427032@ccmailg.meijo-u.ac.jp>
2023-05-10 19:57:37 +0900

    更新

---
+++ shared_dir/api_key.py
@@ -0,0 +1,2 @@
+API_KEY:str = ""
+ORGANIZATION_KEY:str = ""
--- shared_dir/test_cooking.py
+++ shared_dir/test_cooking.py
@@ -1,49 +1,23 @@
-import requests
-import os 
 import openai
+from api_key import API_KEY,ORGANIZATION_KEY
 
+openai.organization = API_KEY
+openai.api_key = ORGANIZATION_KEY
 
-openai.organization = ""
-openai.api_key = ""
+def responce_gpt():
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt='please green pepper cooking',
+        max_tokens=50,
+        temperature=0.7,
+        n=1,
+        stop=None,
+    )
 
-response = openai.Completion.create(
-    engine='text-davinci-003',
-    prompt='Once upon a time',
-    max_tokens=50,
-    temperature=0.7,
-    n=1,
-    stop=None,
-)
+    print(response.choices[0].text.strip())
 
-print(response.choices[0].text.strip())
+    return None
 
-exit()
-
-	
-# Note: you need to be using OpenAI Python v0.27.0 for the code below to work
-openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Who won the world series in 2020?"},
-        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-        {"role": "user", "content": "Where was it played?"}
-    ]
-)
-
-{
- 'id': 'chatcmpl-6p9XYPYSTTRi0xEviKjjilqrWU2Ve',
- 'object': 'chat.completion',
- 'created': 1677649420,
- 'model': 'gpt-3.5-turbo',
- 'usage': {'prompt_tokens': 56, 'completion_tokens': 31, 'total_tokens': 87},
- 'choices': [
-   {
-    'message': {
-      'role': 'assistant',
-      'content': 'The 2020 World Series was played in Arlington, Texas at the Globe Life Field, which was the new home stadium for the Texas Rangers.'},
-    'finish_reason': 'stop',
-    'index': 0
-   }
-  ]
-}
+if __name__ == "__main__":
+    responce_gpt()
+    
