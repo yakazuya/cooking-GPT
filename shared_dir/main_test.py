efaa9a4fcf12e14ed58fdf15715fe882880ca70a
higashi <233427032@ccmailg.meijo-u.ac.jp>
2023-05-21 15:23:59 +0900

    v1

--- shared_dir/main_test.py
+++ shared_dir/main_test.py
@@ -15,13 +15,20 @@ from modules.total_function import *
 UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'gif'])
 
-app = Flask(__name__)
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'gif'])
+
+app = Flask(__name__, static_folder='static')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
+def front():
+    return render_template('front.html')
+
+@app.route('/home', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
@@ -37,10 +44,9 @@ def upload_file():
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             # return redirect(url_for('uploaded_file', filename=filename))
             num = request.form.get('num')  # numの値を取得
-            # img_dict,text_list = predict(file_path,num)
-            all_dict = predict(file_path,num)
+            dict = predict(file_path,num)
             # return predict(file_path,num)
-            return process(all_dict)
+            return process(dict)
 
 
     return render_template('home.html')
@@ -51,8 +57,8 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 @app.route('/process', methods=['GET','POST'])
-def process(all_dict):
-    return render_template('result.html',data=all_dict)
+def process(dict):
+    return render_template('result.html',data=dict)
 
 def predict(filename:str, num:str) -> str:
     # YOLOに入力
@@ -68,10 +74,9 @@ def predict(filename:str, num:str) -> str:
     #           + '4. Potato and Carrot Soup: Sauté onions and garlic in a pot. Add the potatoes and carrots and cook until softened. Add stock and simmer until vegetables are cooked through. Blend until smooth. ' \
     #            + '5. Baked Potato and Carrot Chips: Thinly slice potatoes and carrots and toss with olive oil, salt, and pepper. Bake in a preheated oven at 400°F for 20-25 minutes, flipping halfway through.'
     # text = '1.Stuffed Peppers: Stuff hollowed-out green peppers with a mixture of diced tomatoes, cooked rice, onion, garlic and herbs.' \
-                # '2.Tomato Soup: Simmer tomatoes, green peppers, onion, garlic, and a variety of herbs and spices in a pot of vegetable broth.' \
-                    # '3.Tomato and Pepper Sauté: Sauté chopped tomatoes, peppers, onion, garlic, and herbs in olive oil until vegetables are softened.' \
-                        # '4.Tomato and Pepper Pizza: Spread tomato sauce over pizza dough, top with grated cheese and roasted green peppers and tomatoes.' 
-
+    #             '2.Tomato Soup: Simmer tomatoes, green peppers, onion, garlic, and a variety of herbs and spices in a pot of vegetable broth.' \
+    #                 '3.Tomato and Pepper Sauté: Sauté chopped tomatoes, peppers, onion, garlic, and herbs in olive oil until vegetables are softened.' \
+    #                     '4.Tomato and Pepper Pizza: Spread tomato sauce over pizza dough, top with grated cheese and roasted green peppers and tomatoes.' 
     dishes_list,text_list = dishes_select(text,int(num))
 
     img_list: list = image(dishes_list,text_list)
@@ -98,4 +103,6 @@ def predict(filename:str, num:str) -> str:
 if __name__ == '__main__':
     app.debug = True
     app.run(host='127.0.0.1', port=5000)
-    # img_dict,text_list = predict(filename=None,num=None)
+    # dict = predict(filename=None,num=None)
+    # print(dict)
+    
